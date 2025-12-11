c9b621f5912b651f07d953964e5564e03c116ec0
Marcelo Borges <marcelosbgs@gmail.com>
2025-12-11 12:43:12 -0300

    Repository de Actors

--- actors/repository.py
+++ actors/repository.py
@@ -1,42 +1,47 @@
 import requests
-import streamlit as st
+import streamlit as st 
 from login.service import logout
 
 
-class GenreRepository:
-    """
-    Acessar a API e autenticar a aplicação no Bearer token
-    """
+class ActorRepository:
 
     def __init__(self):
         self.__base_url = 'https://mborges76.pythonanywhere.com/api/v1/'
-        self.__genres_url = f'{self.__base_url}/genres/'
-        self.__headers ={
-            'Authorization': f'Bearer {st.session_state.token}'
+        self.__actors_url = f'{self.__base_url}actors/'
+        self.__headers = {
+                'Authorization': f'Bearer {st.session_state.token}'
         }
 
-    def get_genres(self):
-        """
-        Get nos Generos e checar a validade do token e se vencido, prazo de 1 dia, fará o kikoff do user
-        """
+    def get_actors(self):
         response = requests.get(
-            self.__genres_url, 
-            headers=self.__headers
+                self.__actors_url,
+                headers=self.__headers,
         )
         if response.status_code == 200:
             return response.json()
+        raise Exception(f'Error to recive data from API. Status code {response.status_code}')
         if response.status_code == 401:
             logout()
+            return None
+        raise Exception(f'Error to recive data from API. Status code {response.status_code}')
+    def get_actor(self, actor_id):
+        response = requests.get(
+                self.__actors_url,
+                headers=self.__headers,
+        )
+        if response.status_code == 200:
+            return response.json()
         raise Exception(f'Error to recive data from API. Status code {response.status_code}')
 
-    def create_genre(self, genre):
+
+    def create_actor(self, actor):
         """
         Post nos Generos e checar a validade do token e se vencido, prazo de 1 dia, fará o kikoff do user
         """
         response = requests.post(
-            self.__genres_url, 
+            self.__actors_url, 
             headers=self.__headers,
-            data=genre,
+            data=actor,
         )
         if response.status_code == 201:
             return response.json()
@@ -45,4 +50,4 @@ class GenreRepository:
             return None
         raise Exception(f'Error to recive data from API. Status code {response.status_code}')
 
-        
+
--- genres/page.py
+++ genres/page.py
@@ -5,34 +5,7 @@ Página de generos.
 import pandas as pd
 import streamlit as st
 from st_aggrid import AgGrid
-
-
-genres = [
-    {
-        "id": 1,
-        "name": "Action",
-    },
-    {
-        "id": 2,
-        "name": "Comedy",
-    },
-    {
-        "id": 3,
-        "name": "Drama",
-    },
-    {
-        "id": 4,
-        "name": "Horror",
-    },
-    {
-        "id": 5,
-        "name": "Sci-Fi",
-    },
-    {
-        "id": 6,
-        "name": "Thriller",
-    },
-]
+from genres.service import GenreService
 
 
 def show_genres():
@@ -42,13 +15,34 @@ def show_genres():
     Convertido (provisório, usando dados mocados) a lista de generos para um DataFrame;
     Função será trazer dados da API;
     """
-    AgGrid(
-        data=pd.DataFrame(genres),
-        reload_data=True,
-        key='genres_grid',
-    )
+
+    genre_service = GenreService()
+    genres = genre_service.get_genres()
+
+    if genres:
+        st.write('Genres List: ')
+        """
+        Usando o pandas para converter a lista de generos para um DataFrame;
+        """
+        genres_df = pd.json_normalize(genres)
+        AgGrid(
+            data=genres_df,
+            reload_data=True,
+            key='genres_grid',
+        )
+
+    else:
+        st.warning('No genres found')
+
     st.subheader("Add NewGenre")
     name = st.text_input("Name")
     if st.button("Add"):
-        genres.append({"id": len(genres) + 1, "name": name})
-        st.success(f"Genre {name} added successfully")
+        new_genre = genre_service.create_genre(
+            name=name,
+        )
+        if new_genre:
+            st.success(f"Genre {name} added successfully")
+            st.rerun()
+        else:
+            st.error('Error creating genre')
+

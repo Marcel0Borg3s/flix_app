075c23d4e18d843b1270556eb0894a89c9fef880
Marcelo Borges <marcelosbgs@gmail.com>
2025-12-14 08:35:30 -0300

    atualiza Term SSH

---
+++ movies/repository.py
@@ -0,0 +1,49 @@
+import requests
+import streamlit as st 
+from login.service import logout
+
+
+class MovieRepository:
+
+    def __init__(self):
+        self.__base_url = 'https://mborges76.pythonanywhere.com/api/v1/'
+        self.__actors_url = f'{self.__base_url}movies/'
+        self.__headers ={
+            'Authorization': f'Bearer {st.session_state.token}'
+        }
+
+    def get_movies(self):
+        response = requests.get(
+            self.__movies_url,
+            headers=self.__headers,
+        )
+        if response.status_code == 200:
+            return response.json()
+        raise Exception(f'Error to recive data from API. Status code {response.status_code}')
+        if response == status_code == 401:
+            logout()
+            return None
+        raise Exception(f'Error to recive data from API. Status code {response.status_code}')
+
+    def get_movie(self, movie_id):
+        response = request.get(
+            self.__movies_url,
+            headers=self.__headers,
+        )
+
+        if response.status_code == 200:
+            return response.json()
+        raise Exception(f'Error to recive data from API. Status code {response.status_code}')
+
+    def create_movie(self, movie):
+        response = requests.post(
+        self.__movies_url, 
+        headers=self.__headers,
+        data=actor,
+        )
+        if response.status_code == 201:
+            return response.json()
+        if response.status_code == 401:
+            logout()
+            return None
+        raise Exception(f'Error to recive data from API. Status code {response.status_code}')
